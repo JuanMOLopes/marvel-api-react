6e6384bcda64c2714a38e53514b70714c096a2c3
JuanMOLopes <juan.oliiveira2007@gmail.com>
2025-06-18 09:58:29 -0300

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,14 @@ import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
 import ListaPersonagens from "../components/ListaPersonagens/ListaPersonagens";
+import BuscaHeroi from "../components/BuscaHeroi/BuscaHeroi";
 
 function Home() {
   return (
     <>
       <Header />
+      <BuscaHeroi />
+      <hr />
       <ListaPersonagens />
       <Footer />
     </>
